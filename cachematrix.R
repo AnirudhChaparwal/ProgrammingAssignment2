a57a72a2f3a913ad2717ddc44142f668db5dfb27
Anirudh <anirudhchaparwal@gmail.com>
2020-04-29 16:40:15 +0530

    1st commit for PA2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special matrix object that cache it's inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse<-NULL
+  set<-function(y){
+    x<<-y
+    inverse<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(solve) inverse<-solve
+  getinverse<-function() inverse
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## This function computes the inverse of the special matrix returned by the above functionmakeCacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse<-x$getinverse()
+  if(!is.null(inverse)){
+    message("Getting cached inverse of matrix")
+    return(inverse)
+  }
+  data<-x$get()
+  inverse<-solve(data)
+  x$setinverse(inverse)
+  inverse
 }
